a35f95b966e0246a6b71e4b2fae6cdcd454b1fb0
Nick Steinhauser <nicksteinhauser@gmail.com>
2017-12-18 17:19:25 -0600

    completed simplified version

--- steinhauser_sakila_w_views.sql
+++ steinhauser_sakila_w_views.sql
@@ -1,5 +1,37 @@
 use sakila;
 select * from staff;
+#housekeeping views
+#all pertinent location ids
+create view location_ids as
+(select kk.*, staff_id from staff right join
+(select jj.*, store_id from store right join
+(select ii.*,customer_id from customer right join
+(select address_id, hh.* from address  left join
+(select city, city_id, country, city.country_id from city left join country 
+on city.country_id = country.country_id) hh
+on address.city_id = hh.city_id) ii
+on ii.address_id = customer.address_id) jj
+on jj.address_id = store.address_id) kk
+on kk.address_id = staff.address_id);
+
+#packing the movie id info in
+create view actor_ids as
+select first_name, last_name, jj.* from actor right join
+(select actor_id, ii.* from film_actor left join
+(select film.film_id, title, language_id, category_id from film left join film_category 
+on film.film_id = film_category.film_id) ii
+on ii.film_id = film_actor.film_id) jj
+on jj.actor_id = actor.actor_id;
+
+create view rental_ids as
+select rental_id, customer_id, staff_id, jj.* from rental left join
+(select inventory_id, store_id, ii.* from inventory left join
+(select film.film_id, title, language_id, category_id from film left join film_category 
+on film.film_id = film_category.film_id) ii
+on inventory.film_id = ii.film_id) jj
+on rental.inventory_id = jj.inventory_id;
+
+
 
 # 1a. Display the first and last names of all actors from the table `actor`. 
 select first_name, last_name from actor;
@@ -20,7 +52,6 @@ select * from actor where last_name like "%GEN%" order by last_name, first_name;
 select country_id, country from country where country in ("Afghanistan", "Bangladesh", "China");
 
 # 3a. Add a `middle_name` column to the table `actor`. Position it between `first_name` and `last_name`. Hint: you will need to specify the data type.
-alter table actor drop column middle_name;
 alter table actor add middle_name VARCHAR(20) default "FRANCIS" After first_name;
 select * from actor;
 
@@ -35,10 +66,11 @@ select last_name, count(last_name) from actor group by last_name;
   	
 # 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
 select * from
-(select last_name, count(last_name) `count` from actor  group by last_name) as t
-where (`count` > 2);
+(select last_name, count(last_name) `count of last name` from actor  group by last_name) as t
+where (`count of last name` >= 2);
+
 # 4c. Oh, no! The actor `HARPO WILLIAMS` was accidentally entered in the `actor` table as `GROUCHO WILLIAMS`, the name of Harpo's second cousin's husband's yoga teacher. Write a query to fix the record.
-select * from actor where last_name = "WILLIAMS";
+select * from actor where last_name = "WILLIAMS"; #actor_id = 172
 
 update actor set first_name = "HARPO"
 where actor_id = 172;
@@ -74,11 +106,11 @@ select * from staff left join address on staff.address_id = address.address_id;
 
 # 6b. Use `JOIN` to display the total amount rung up by each staff member in August of 2005. Use tables `staff` and `payment`. 
 #select staff_id
-select * from staff left join
-(select staff_id, sum(amount) from payment 
+select first_name, last_name, `$` from staff left join
+(select staff_id, sum(amount) `$` from payment 
 where payment_date between '2005-08-01 00:00:00' AND '2005-08-31 23:59:59'
-group by(staff_id)) sums
-on staff.staff_id = sums.staff_id;
+group by(staff_id)) date_sums
+on staff.staff_id = date_sums.staff_id;
 
 # 6c. List each film and the number of actors who are listed for that film. Use tables `film_actor` and `film`. Use inner join.
 select title, counts.actor_count  from film left join
@@ -86,8 +118,7 @@ select title, counts.actor_count  from film left join
 on film.film_id = counts.film_id;
     
 # 6d. How many copies of the film `Hunchback Impossible` exist in the inventory system?
-select * from film where title = "Hunchback Impossible"; # id = 439
-select count(inventory_id) `Copies of Hunchback Impossible` from inventory where film_id = 439 group by(film_id);
+select count(distinct inventory_id) `Copies of Hunchback Impossible` from rental_ids where title = "Hunchback Impossible";
 
 # 6e. Using the tables `payment` and `customer` and the `JOIN` command, list the total paid by each customer. List the customers alphabetically by last name:
 select first_name, last_name, customer_sums.customer_sum from customer join
@@ -99,72 +130,44 @@ select first_name, last_name, customer_sums.customer_sum from customer join
 select title from film where title like "K%" or title like "Q%" and language_id = 1;
 
 # 7b. Use subqueries to display all actors who appear in the film `Alone Trip`.
-select first_name, last_name from actor where actor_id in
-	(select actor_id from film_actor where film_id in
-	(select film_id from film where title = "Alone Trip"));
+select title, actors from nicer_but_slower_film_list where title = "Alone Trip";
+
+select first_name, last_name from actor_ids where title = "Alone Trip";
 
 # 7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
 select email from customer where address_id in
-	(select address_id from address where city_id in
-	(select city_id from city where country_id in
-	(select country_id from country where country = "CANADA")));
+	(select address_id from location_ids where country = "CANADA");
 
 # 7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as famiy films.
-select * from film where film_id in
-(select film_id from film_category where category_id in
-(select category_id from category where name = "Family"));
+select FID, title from film_list where category = "family";
 
 # 7e. Display the most frequently rented movies in descending order.
-select title,k.`# of rentals` from film join
-(select film_id, count(rental_id) `# of rentals` from
-(select rental_id, film_id from rental left join inventory on inventory.inventory_id = rental.inventory_id) j
-group by(film_id)) k
-on k.film_id = film.film_id
-order by `# of rentals` desc;
+select title, count(rental_id) `rentals` from rental_ids 
+group by(film_id) order by rentals desc;
 
 # 7f. Write a query to display how much business, in dollars, each store brought in.
-select store_id, sum(`1st`.amount) `$` from staff join
-(select staff_id, amount from payment) `1st` 
-where staff.staff_id = `1st`.staff_id
-group by store_id;
+select * from sales_by_store;
 
 
 # 7g. Write a query to display for each store its store ID, city, and country.
-select store_id, city, country from store join
-(select city, country, address_id from address join
-(select city_id, city, country from city join country on city.country_id = country.country_id) ii
-on address.city_id = ii.city_id) jj
-on store.address_id = jj.address_id;
-
+select store.store_id, city, country 
+from store left join location_ids 
+on store.store_id = location_ids.store_id;
 
 # 7h. List the top five genres in gross revenue in descending order. (**Hint**: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
-select `name`, sum(amount) `$` from category right join
-(select amount, category_id from film_category right join
-(select amount, film_id from inventory right join
-(select amount, inventory_id from payment left join rental
-on rental.rental_id = payment.rental_id) ii
-on inventory.inventory_id = ii.inventory_id) jj
-on film_category.film_id = jj.film_id) kk
-on category.category_id = kk.category_id
-group by(kk.category_id)
-order by `$` desc;
-
+select * from sales_by_film_category
+order by total_sales desc #which is default anyway
+limit 5;
 
 # 8a. In your new role as an executive, you would like to have an easy way of viewing the Top five genres by gross revenue. Use the solution from the problem above to create a view. If you haven't solved 7h, you can substitute another query to create a view.
-create view genre (`genre`,`$`) as
-(select `name`, sum(amount) `$` from category right join
-(select amount, category_id from film_category right join
-(select amount, film_id from inventory right join
-(select amount, inventory_id from payment left join rental
-on rental.rental_id = payment.rental_id) ii
-on inventory.inventory_id = ii.inventory_id) jj
-on film_category.film_id = jj.film_id) kk
-on category.category_id = kk.category_id
-group by(kk.category_id)
-order by `$` desc);
+
+create view genre_top_5 as
+select * from sales_by_film_category
+order by total_sales desc #which is default anyway
+limit 5;
 
 # 8b. How would you display the view that you created in 8a?
-select * from genre;
+select * from genre_top_5;
 
 # 8c. You find that you no longer need the view `top_five_genres`. Write a query to delete it.
-drop view genre;
+drop view genre_top_5;
